085162c6d3fee131d4bd07d99bfa7d480554c533
Shreya Rawat <108164860+Shreya-Rawat@users.noreply.github.com>
2026-02-23 12:06:16 +0530

    Update llm_model to use qwen2:7b

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 model_name="sentence-transformers/all-MiniLM-L6-v2"
-upload_file_url = 'http://localhost:8000' 
-llm_model = 'llama3.2:1b'
+upload_file_url = 'http://localhost:8000'
+llm_model = 'qwen2:7b'
